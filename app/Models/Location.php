6169207bf132cea0aeb793b6a4687ee3b9c7bbef
Semet <hamdanilombok@gmail.com>
2023-11-16 17:59:56 +0800

    update location model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Location extends Model
 {
@@ -14,4 +15,9 @@ class Location extends Model
         'name',
         'address'
     ];
+
+    public function properties(): HasMany
+    {
+        return $this->hasMany(Property::class);
+    }
 }
